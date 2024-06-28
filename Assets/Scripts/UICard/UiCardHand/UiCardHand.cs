6f4b44d0aef81ca47cd5d4cbbc7afa10f2170bab
Declan Clowry <declanclowry@Declans-MacBook-Pro.local>
2024-06-28 21:05:16 +0900

    restore mulawin changed

--- Assets/Scripts/UICard/UiCardHand/UiCardHand.cs
+++ Assets/Scripts/UICard/UiCardHand/UiCardHand.cs
@@ -43,7 +43,42 @@ namespace Tools.UI.Card
         /// <summary>
         ///     Event raised when a card is selected.
         /// </summary>
-	@@ -91,37 +80,6 @@ public void PlayCard(IUiCard card)
+        Action<IUiCard> IUiCardHand.OnCardSelected{ get => OnCardSelected; set => OnCardSelected = value; }
+        #endregion
+        //--------------------------------------------------------------------------------------------------------------
+        #region Operations
+        /// <summary>
+        ///     Select the card in the parameter.
+        /// </summary>
+        /// <param name="card"></param>
+        public void SelectCard(IUiCard card)
+        {
+            SelectedCard = card ?? throw new ArgumentNullException("Null is not a valid argument.");
+            //disable all cards
+            DisableCards();
+            NotifyCardSelected();
+        }
+        /// <summary>
+        ///     Play the card which is currently selected. Nothing happens if current is null.
+        /// </summary>
+        /// <param name="card"></param>
+        public void PlaySelected()
+        {
+            if (SelectedCard == null)
+                return;
+            PlayCard(SelectedCard);
+        }
+        /// <summary>
+        ///     Play the card in the parameter.
+        /// </summary>
+        /// <param name="card"></param>
+        public void PlayCard(IUiCard card)
+        {
+            if (card == null)
+                throw new ArgumentNullException("Null is not a valid argument.");
+            SelectedCard = null;
+            RemoveCard(card);
+            OnCardPlayed?.Invoke(card);
             EnableCards();
             NotifyPileChange();
         }
